e3d1ff5ab131ab7b829a9e0eecc53d47fb3795b8
BHM <maxtty@naver.com>
2026-01-28 16:14:44 +0900

    uplode

---
+++ js/app.js
@@ -0,0 +1,11 @@
+ var swiper = new Swiper(".gallery", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    navigation: {
+        nextEl: ".gallery .swiper-button-next",
+        prevEl: ".gallery .swiper-button-prev",
+      },
+    loop: true,
+    // autoplay: true     
+  });
